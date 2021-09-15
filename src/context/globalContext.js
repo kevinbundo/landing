4c3b831bff76b4f1f5a33d62e5b3773b164c41d7
kevinbundo <kevin_bundo@yahoo.com>
2021-09-15 18:06:28 +0200

    globalContext

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -26,18 +26,15 @@ const globalReducer = (state, action) => {
 }
 
 export const GlobalProvider = ({ children }) => {
-  if (typeof window !== `undefined`) {
   const [state, dispatch] = useReducer(globalReducer, {
     currentTheme:
-   
       window.localStorage.getItem("theme") == null
         ? "dark"
         : window.localStorage.getItem("theme"),
-    
+
     cursorType: false,
     cursorStyles: ["pointer", "hovered", "locked"],
-  
-  })}
+  })
   return (
     <GlobalDispatchContext.Provider value={dispatch}>
       <GlobalStateContext.Provider value={state}>
